1cd819ed508e61b554a5514e6c5e23879197d9c3
xplshn <114888778+xplshn@users.noreply.github.com>
2024-07-24 05:36:16 -0300

    Update cbuild.sh

--- cbuild.sh
+++ cbuild.sh
@@ -1,72 +1,89 @@
 #!/bin/sh
 
-OPWD="$PWD"
-BASE="$(dirname "$(realpath "$0")")"
+POSIXLY_CORRECT=1
+cbuild_OPWD="$PWD"
+BASE="$(realpath "$0")" && BASE="${BASE%/*}"
 if [ "$OPWD" != "$BASE" ]; then
-	echo "... $BASE is not the same as $PWD ..."
-	echo "Going into $BASE and coming back here in a bit"
-	cd "$BASE" || exit 1
+    cd "$BASE" || log "$R" "Unable to change directory to ${BASE##*/}. Re-execute using a POSIX shell and check again."
 fi
-trap 'cd "$OPWD"' EXIT
+trap 'cd "$cbuild_OPWD"' EXIT
 
-# Function to log to stdout with green color
+# Color escape sequences
+G="\033[32m" #     Green
+R="\033[31m" #     Red
+B="\033[34m" #     Blue
+NC="\033[m"  #     Unset
+
 log() {
-	_Xashstd_reset="\033[m"
-	_Xashstd_color_code="\033[32m"
-	printf "${_Xashstd_color_code}->${_Xashstd_reset} %s\n" "$*"
+    # shellcheck disable=SC2059 # Using %s with ANSII escape sequences is not possible
+    printf "${1}->$NC "
+    shift
+    printf "%s\n" "$*"
 }
 
-# Function to log_warning to stdout with yellow color
-log_warning() {
-	_Xashstd_reset="\033[m"
-	_Xashstd_color_code="\033[33m"
-	printf "${_Xashstd_color_code}->${_Xashstd_reset} %s\n" "$*"
-}
-
-# Function to log_error to stdout with red color
-log_error() {
-	_Xashstd_reset="\033[m"
-	_Xashstd_color_code="\033[31m"
-	printf "${_Xashstd_color_code}->${_Xashstd_reset} %s\n" "$*"
-	exit 1
-}
-
-unnappear() {
-	"$@" >/dev/null 2>&1
-}
-
-# Check if a dependency is available.
-available() {
-	unnappear which "$1" || return 1
-}
-
-# Exit if a dependency is not available
 require() {
-	available "$1" || log_error "[$1] is not installed. Please ensure the command is available [$1] and try again."
+    command -v "$1" >/dev/null 2>&1 || {
+        log "$R" "[$1] is not installed. Please ensure the command is available [$1] and try again."
+        exit 1
+    }
 }
 
-case "$1" in
-"" | "build")
-	require "${CC:=cc}"
-	require make
-	require bison
-	log "Using make to build \"$(basename "$BASE")\""
-	make -j"$(nproc)" || log_error "Make command failed" && {
-		mv ./a.out ./awk
-	}
-	;;
-"clean")
-	shift
-	log "Starting clean process"
-	make clean
-	unnappear rm ./a.out ./awk
-	log "Clean process completed"
-	;;
-"retrieve")
-	readlink -f ./awk
-	;;
-*)
-	echo "Usage: $0 {build|clean}"
-	exit 1
-	;;
-esac
+run() {
+    log "$B" "$*"
+    # shellcheck disable=SC2068 # We want to split elements, but avoid whitespace problems (`$*`), and also avoid `eval $*`
+    $@
+}
+
+: "${CC:=cc}"
+build() {
+    require "make"
+    require "${CC}"
+    log "$G" "Entering step: \"Build \"${BASE##*/}\" using \"$CC\""
+    run "make" || {
+        log "$R" "Failed during step: \"Build \"${BASE##*/}\" using \"$CC\""
+        exit 1
+    }
+}
+
+# Argument processing
+while [ $# -gt 0 ] || [ "$1" = "" ]; do
+    case "$1" in
+    "install")
+        shift
+        build && {
+            run mkdir -p "$DESTDIR$PREFIX/bin/" &&
+                run cp -f vi "$DESTDIR$PREFIX/bin/vi" &&
+                [ -x "$DESTDIR$PREFIX/bin/vi" ] && log "$G" "\"${BASE##*/}\" has been install to $DESTDIR$PREFIX/bin/vi" || log "$R" "Couldn't finish installation"
+        } && exit 0 || exit 1
+        ;;
+    "" | "build")
+        # If the user doesn't use "build" explicitely, do not run the build step again.
+        [ "$1" = "build" ] || {
+            explicit="1"
+        } && [ -n "$1" ] && shift
+        if [ "$explicit" = "1" ]; then
+            [ -f ./vi ] || [ -f ./nextvi ] && log "$R" "Nothing to do; \"${BASE##*/}\" was already compiled" && exit 0
+        fi
+        # Start build process
+        build && exit 0 || exit 1
+        ;;
+    "clean")
+        shift
+        run rm ./vi ./nextvi 2>/dev/null
+        exit 0
+        ;;
+    "retrieve")
+        shift
+        [ -x "./a.out" ] || [ -x ."/awk" ] && {
+            log "$R" "\"${BASE##*/}\" was never compiled OR it was but its binaries weren't found anyways."
+            exit 1
+        } && mv ./a.out ./awk
+        readlink -f ./awk
+        exit 0
+        ;;
+    *)
+        echo "Usage: $0 {install|pgobuild|build|debug|clean}"
+        exit 1
+        ;;
+    esac
+done
